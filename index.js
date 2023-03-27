08f3eb843d85093b028402eac0756a766f4c9dfe
Soungjae <soungj94@gmail.com>
2023-03-27 13:47:16 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-// Write your code here!
-// Remove the main element with the id "main"
-document.querySelector("main#main").remove();
-
-// Create a new h1 element and set its text content
+const deleteNode = document.querySelector("main#main");
+deleteNode.remove("main#main");
 const newHeader = document.createElement("h1");
 newHeader.textContent = "YOUR-NAME is the champion";
-
-// Set the id of the newHeader element to 'victory'
 newHeader.setAttribute("id", "victory");
-
-// Append the newHeader element to the body of the HTML document
 document.body.appendChild(newHeader);
